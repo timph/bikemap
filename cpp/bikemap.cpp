7adb2313b99bf9fd62453d61bdafb6d799c67389
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-09-28 14:26:44 -0700

    cpp: Add .clang-format file
    It's convenient to have a code formatter while developing, so that it
    can fix up the syntax on save.
    
    Many IDEs / editor tools rely on the presence of a `.clang-format` file
    to enable autoformating files on save. Adding this file makes it easier
    for candidates to clang-format, while not hurting candidates who haven't
    set up their development environment to use this.
    
    I've added the `.clang-format` file that we use in
    <https://github.com/sorbet/sorbet> and reformatted the starter code
    according to it.

--- cpp/bikemap.cpp
+++ cpp/bikemap.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
-#include "json/json.hpp"
+
 #include "cpr/cpr.h"
+#include "json/json.hpp"
 
 void handle_json() {
-  using nlohmann::json;
-  // create from string
-  json j = json::parse("{\"foo\": {\"bar\": [{\"nato\": \"oscar\"}, {\"nato\": \"papa\"}, {\"nato\": \"quebec\"}]}}");
+    using nlohmann::json;
+    // create from string
+    json j = json::parse("{\"foo\": {\"bar\": [{\"nato\": \"oscar\"}, {\"nato\": \"papa\"}, {\"nato\": \"quebec\"}]}}");
 
-  // explicit serialization to string
-  std::string s = j.dump(); 
-  std::cout << ".dump() " << s << std::endl;
+    // explicit serialization to string
+    std::string s = j.dump();
+    std::cout << ".dump() " << s << std::endl;
 
-  // access specific value using JSON Pointer spec (https://tools.ietf.org/html/rfc6901)
-  auto nato_letter = j["/foo/bar/1/nato"_json_pointer];
-  std::cout << ".get() " << nato_letter.get<std::string>() << std::endl;
+    // access specific value using JSON Pointer spec (https://tools.ietf.org/html/rfc6901)
+    auto nato_letter = j["/foo/bar/1/nato"_json_pointer];
+    std::cout << ".get() " << nato_letter.get<std::string>() << std::endl;
 
-  // create a new json object and add it to the prev one
-  json j2;
-  // add a string
-  j2["stuff"] = "nonsense";
-  // add an array that is stored as std::vector
-  j2["nums"] = {2.718, 3.142};
-  // add j2 to j
-  j["foo"]["quux"] = j2;
+    // create a new json object and add it to the prev one
+    json j2;
+    // add a string
+    j2["stuff"] = "nonsense";
+    // add an array that is stored as std::vector
+    j2["nums"] = {2.718, 3.142};
+    // add j2 to j
+    j["foo"]["quux"] = j2;
 
-  // pretty print by passing an int for indentation to dump
-  std::cout << j.dump(4);
+    // pretty print by passing an int for indentation to dump
+    std::cout << j.dump(4);
 }
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello World!" << std::endl;
-  handle_json();
-  return 0;
+    std::cout << "Hello World!" << std::endl;
+    handle_json();
+    return 0;
 }
-
